0dc148bab54dcd23ece180b93504611fd95c84fd
Armleth <armand.thibaudon@epita.fr>
2026-01-16 19:02:49 +0100

    feat: addition of second tp notebook

---
+++ practical-generating-molecules/flake.nix
@@ -0,0 +1,40 @@
+{
+    inputs = {
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs =
+        {
+            self,
+            nixpkgs,
+            flake-utils,
+        }:
+        flake-utils.lib.eachDefaultSystem (
+            system:
+            let
+                pkgs = nixpkgs.legacyPackages.${system};
+                rs-python = pkgs.python312.withPackages (
+                    ps: with ps; [
+                        numpy
+                        torch
+                        torchvision
+                        pandas
+                        pillow
+
+                        notebook
+                        ipykernel
+                        ipywidgets
+                    ]
+                );
+            in
+            with pkgs;
+            {
+                devShells.default = mkShell {
+                    buildInputs = [
+                        rs-python
+                        jupyter-all
+                    ];
+                };
+            }
+        );
+}
